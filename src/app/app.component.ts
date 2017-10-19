aeab10b2bea782d3316f44d63f0196c10466365b
Yesenia <eliyessie@gmail.com>
2017-10-18 17:00:12 -0700

    styled page added movie lists, still not displaying properly. Will continue to work on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = ' the Movies';
+  movieList: Array<Movie>;
+  constructor() {}
+
+  ngOnInit(){ //use real movie titles so that you can call thumbnails and even call the trailers ---
+    this.movieList = [
+      {
+      title: 'Baywatch',
+      released_on: '2017',
+      thumbnail: '../app/images/baywatch.jpg',
+      trailer: 'https://youtu.be/nZ5tqzw841s',
+      },
+      {
+      title: 'Transformers',
+      released_on: '2017',
+      thumbnail: '../app/images/transformers.jpg',
+      trailer: 'https://youtu.be/6Vtf0MszgP8',
+      },
+      {
+      title: 'Boss Baby',
+      released_on: '2017',
+      thumbnail: '../app/images/bossbaby.jpg',
+      trailer: 'https://youtu.be/Ud8j5GaqH3c',
+      },
+      {
+      title: 'Wonder Woman',
+      released_on: '2017',
+      thumbnail: '../app/images/wonderwoman.jpg',
+      trailer: 'https://youtu.be/VSB4wGIdDwo',
+      },
+    ]
+  }
+}
+
+
+export class Movie {
+  title: string;
+  released_on: string;
+  trailer: string;
+  thumbnail: string;
 }
